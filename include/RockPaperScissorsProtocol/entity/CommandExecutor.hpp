92879bdda87c150cd2db14b96a32bd0d7b0a842b
root-kidik <rtkid.nik@gmail.com>
2025-02-04 17:51:23 +0700

    feat: accept in executor raw_data

--- include/RockPaperScissorsProtocol/entity/CommandExecutor.hpp
+++ include/RockPaperScissorsProtocol/entity/CommandExecutor.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 
+#include <RockPaperScissorsProtocol/entity/CommandRepresentation.hpp>
 #include <RockPaperScissorsProtocol/interface/CommandHandlerBase.hpp>
 #include <RockPaperScissorsProtocol/interface/Connection.hpp>
 
@@ -15,9 +16,24 @@ template <typename CommandType>
 class CommandExecutor
 {
 public:
-    void execute_command(CommandType command_type, std::string&& data, const std::shared_ptr<interface::Connection>& connection)
+    void execute_command(std::string&& raw_data, const std::shared_ptr<interface::Connection>& connection)
     {
-        auto it = m_commands.find(command_type);
+        std::istringstream iss{std::move(raw_data)};
+
+        entity::CommandRepresentation command_type;
+        iss >> command_type;
+
+        if (command_type < static_cast<entity::CommandRepresentation>(CommandType::Begin) + 1 ||
+            command_type > static_cast<entity::CommandRepresentation>(CommandType::End) - 1)
+            return;
+
+        std::string data;
+        std::getline(iss, data);
+
+        if (!data.empty() && std::isspace(data.front()))
+            data.erase(0, 1);
+
+        auto it = m_commands.find(static_cast<CommandType>(command_type));
 
         assert(it != m_commands.end() && "Not setted command to execute this command_type");
 
