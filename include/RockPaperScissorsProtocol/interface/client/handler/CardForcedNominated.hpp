25c348962bc81954667b592c8d67f4fc68398ffb
root-kidik <rtkid.nik@gmail.com>
2025-02-09 16:56:40 +0700

    fix: client side

--- include/RockPaperScissorsProtocol/interface/client/handler/CardForcedNominated.hpp
+++ include/RockPaperScissorsProtocol/interface/client/handler/CardForcedNominated.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <RockPaperScissorsProtocol/entity/client/request/CardForcedNominated.hpp>
-#include <RockPaperScissorsProtocol/entity/client/response/StatusResponse.hpp>
+#include <RockPaperScissorsProtocol/entity/client/response/Status.hpp>
 #include <RockPaperScissorsProtocol/interface/CommandHandler.hpp>
 
 namespace rps::protocol::interface::client::handler
 {
 
-using CardForcedNominated = CommandHandler<entity::client::CardForcedNominated, entity::client::StatusResponse>;
+using CardForcedNominated = CommandHandler<entity::client::request::CardForcedNominated, entity::client::response::Status>;
 
 } // namespace rps::protocol::interface::client::handler
