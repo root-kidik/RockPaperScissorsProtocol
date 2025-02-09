891da38979b208a44c9c7a0ed68ad4e0d23fc9d0
root-kidik <rtkid.nik@gmail.com>
2025-02-09 22:42:03 +0700

    fix: correct response handler for Register

--- include/RockPaperScissorsProtocol/interface/client/handler/response/Register.hpp
+++ include/RockPaperScissorsProtocol/interface/client/handler/response/Register.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <RockPaperScissorsProtocol/entity/server/request/Register.hpp>
-#include <RockPaperScissorsProtocol/entity/server/response/Status.hpp>
+#include <RockPaperScissorsProtocol/entity/server/response/Register.hpp>
 #include <RockPaperScissorsProtocol/interface/ResponseHandler.hpp>
 
 namespace rps::protocol::interface::client::handler::response
 {
 
-using Register = ResponseHandler<entity::server::response::Status<entity::server::request::Register>>;
+using Register = ResponseHandler<entity::server::response::Register>;
 
 } // namespace rps::protocol::interface::client::handler::response
