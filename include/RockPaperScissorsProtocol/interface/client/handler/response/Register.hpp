1ed7457d51b95b3fda88384c5419d8e3e10d6f08
root-kidik <rtkid.nik@gmail.com>
2025-02-09 21:46:48 +0700

    chore: format

--- include/RockPaperScissorsProtocol/interface/client/handler/response/Register.hpp
+++ include/RockPaperScissorsProtocol/interface/client/handler/response/Register.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <RockPaperScissorsProtocol/entity/server/request/StartGame.hpp>
+#include <RockPaperScissorsProtocol/entity/server/request/Register.hpp>
 #include <RockPaperScissorsProtocol/entity/server/response/Status.hpp>
 #include <RockPaperScissorsProtocol/interface/ResponseHandler.hpp>
 
 namespace rps::protocol::interface::client::handler::response
 {
 
-using StartGame = ResponseHandler<entity::server::response::Status<entity::server::request::StartGame>>;
+using Register = ResponseHandler<entity::server::response::Status<entity::server::request::Register>>;
 
 } // namespace rps::protocol::interface::client::handler::response
