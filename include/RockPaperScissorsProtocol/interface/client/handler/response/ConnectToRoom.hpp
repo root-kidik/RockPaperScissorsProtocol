af670dc2b76169c6a209c40a2a0f9184893e0e01
root-kidik <rtkid.nik@gmail.com>
2025-02-14 00:33:28 +0700

    fix: client response handler

--- include/RockPaperScissorsProtocol/interface/client/handler/response/ConnectToRoom.hpp
+++ include/RockPaperScissorsProtocol/interface/client/handler/response/ConnectToRoom.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <RockPaperScissorsProtocol/entity/server/request/ConnectToRoom.hpp>
-#include <RockPaperScissorsProtocol/entity/server/response/Status.hpp>
+#include <RockPaperScissorsProtocol/entity/server/response/ConnectToRoom.hpp>
 #include <RockPaperScissorsProtocol/interface/ResponseHandler.hpp>
 
 namespace rps::protocol::interface::client::handler::response
 {
 
-using ConnectToRoom = ResponseHandler<entity::server::response::Status<entity::server::request::ConnectToRoom>>;
+using ConnectToRoom = ResponseHandler<entity::server::response::ConnectToRoom>;
 
 } // namespace rps::protocol::interface::client::handler::response
