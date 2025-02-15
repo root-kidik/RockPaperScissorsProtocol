42fc1a1f3bdba2968609385ee5e0cd9c9d478a17
root-kidik <rtkid.nik@gmail.com>
2025-02-15 17:36:16 +0700

    chore: make format

--- include/RockPaperScissorsProtocol/entity/MessageExecutor.hpp
+++ include/RockPaperScissorsProtocol/entity/MessageExecutor.hpp
@@ -63,7 +63,7 @@ private:
     void execute_message(std::string&& raw_data, const std::shared_ptr<interface::Connection>& connection)
     {
 #ifndef NDEBUG
-        std::cout << "receive" << raw_data;
+        std::cout << "receive: " << raw_data;
 #endif
 
         std::istringstream iss{std::move(raw_data)};
--- include/RockPaperScissorsProtocol/entity/server/response/ConnectToRoom.hpp
+++ include/RockPaperScissorsProtocol/entity/server/response/ConnectToRoom.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <string>
 #include <array>
+#include <string>
 
-#include <RockPaperScissorsProtocol/entity/server/ServerMessage.hpp>
 #include <RockPaperScissorsProtocol/entity/Card.hpp>
+#include <RockPaperScissorsProtocol/entity/server/ServerMessage.hpp>
 
 namespace rps::protocol::entity::server::response
 {
 
 struct ConnectToRoom final : ServerMessage<ServerMessageType::ConnectToRoom>
 {
-    bool is_ok;
+    bool                                            is_ok;
     std::array<std::string, kMaxPlayersPerRoom - 1> existed_players;
 
     auto as_tuple()
