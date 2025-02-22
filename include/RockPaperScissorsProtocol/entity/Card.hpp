25ae5263842fc46607e425458852aba3fab1d7fb
root-kidik <rtkid.nik@gmail.com>
2025-02-22 14:56:37 +0700

    feat: change constants

--- include/RockPaperScissorsProtocol/entity/Card.hpp
+++ include/RockPaperScissorsProtocol/entity/Card.hpp
@@ -6,10 +6,10 @@
 namespace rps::protocol::entity
 {
 
-inline constexpr std::uint8_t         kMaxCardsPerPlayer{6};
-inline constexpr std::uint8_t         kMaxCardsPerRoom{36};
+inline constexpr std::uint8_t         kMaxCardsPerPlayer{4};
+inline constexpr std::uint8_t         kMaxCardsPerRoom{16};
 inline constexpr std::uint8_t         kMaxPlayersPerRoom{4};
-inline constexpr std::chrono::seconds kTurnTime{10};
+inline constexpr std::chrono::seconds kTurnTime{5};
 
 using CardRepresentation = std::uint32_t;
 
